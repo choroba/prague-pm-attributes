993c8d40492cada58ca1cc98106071fce67576cf
E. Choroba <choroba@matfyz.cz>
2016-01-27 11:54:58 +0100

    Suggested changes
    - push to @ISA only once
    - CODE as the hash key

--- Export/Attr.pm
+++ Export/Attr.pm
@@ -11,16 +11,22 @@ use strict;
 
 use Attribute::Handlers;
 
+
+my %pushed;
 sub UNIVERSAL::Export :ATTR(BEGIN) {
     my ($package, $ref, $type) = @_;
     (my $symbol = $$ref) =~ s/^.*:://;
     my $sigil = { SCALAR => '$',
                   ARRAY  => '@',
                   HASH   => '%',
-                }->{ref $type} || q();
+                  CODE   => q(),
+                }->{ref $type};
 
     {   no strict 'refs';
-        push @{"$package\::ISA"}, 'Exporter';
+        if (! $pushed{$package}) {
+            push @{"$package\::ISA"}, 'Exporter';
+            $pushed{$package} = 1;
+        }
         push @{"$package\::EXPORT"}, $sigil . $symbol;
     }
 
